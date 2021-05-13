c622bf338a3f5b105d36f423004e1a1740b21e29
Usuário <Usurio@192.168.15.5>
2021-05-13 16:16:15 -0400

    Aluno Persiste

--- POO2- Modelo de Persistencia/src/AlunoPersist.java
+++ POO2- Modelo de Persistencia/src/AlunoPersist.java
@@ -1,4 +1,15 @@
 
+
 public class AlunoPersist {
 
+	private IAlunoPersist persist;
+	
+	public AlunoPersist(IAlunoPersist persist) {
+		this.persist = persist;
+	}
+	
+	public void gravar(Aluno alu) {
+		persist.gravar(alu);
+	}
+	
 }
--- POO2- Modelo de Persistencia/src/Main.java
+++ POO2- Modelo de Persistencia/src/Main.java
@@ -1,15 +1,22 @@
 import java.io.IOException;
 import java.util.Iterator;
 
+
+
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
-		Aluno al1 = new Aluno("123424","2341234", "12/05", "qweqwe@gmais.com", "Rafael");
-		System.out.println(al1);
-	
-		JSON ArmaJson = new JSON ();
-		ArmaJson.ArmazenarJSON("123424","2341234", "12/05", "qweqwe@gmais.com", "Rafael");
-		ArmaJson.toString();
+		
+		Aluno alu = new Aluno("123424","2341234", "12/05", "qweqwe@gmais.com", "Rafael");
+		
+		AlunoCSV a_csv = new AlunoCSV();
+		AlunoPersist persist = new AlunoPersist(a_csv);
+		persist.gravar(alu);
+		
+		AlunoJSON a_json = new AlunoJSON();
+		persist = new AlunoPersist(a_json);
+		persist.gravar(alu);
+		
 	}
 }
